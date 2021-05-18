9111e0a30f93e5956e43f65931aa9e075d8678f9
David Wu <david204code@gmail.com>
2021-05-18 20:19:09 +0100

    set margin to 0 and rought plan for the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div className="app">Peace to you</div>;
+  return (
+    <div className="app">
+      <h1>Peace to you</h1>
+      <p>top</p>
+      <p>top -intro -portfolio -work ...</p>
+      <p>sec -intro -portfolio -work ...</p>
+      <p>-intro -portfolio -work ...</p>
+      <p>-portfolio -work ...</p>
+      <p>-work ...</p>
+    </div>
+  );
 }
 
 export default App;
