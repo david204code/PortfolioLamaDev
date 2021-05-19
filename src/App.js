58fe45531632192d79f55a1315b2acf405686b15
David Wu <david204code@gmail.com>
2021-05-19 17:39:09 +0100

    Added the Topbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Topbar from "./components/topbar/Topbar";
+
 function App() {
   return (
     <div className="app">
+      <Topbar />
       <h1>Peace to you</h1>
       <p>top</p>
       <p>top -intro -portfolio -work ...</p>
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./topbar.scss";
+
+export default function topbar() {
+  return <div className="topbar"></div>;
+}
