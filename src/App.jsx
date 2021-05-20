cee5c83a9ad59f5a7bc674807522d9e0e4f7972f
David Wu <david204code@gmail.com>
2021-05-20 19:09:03 +0100

    Displayed all components in a single page(height:100vh) and hidden the overflow-x on style of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Portforlio from "./components/portfolio/Portfolio";
 import Works from "./components/work/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import "./app.scss";
 
 function App() {
   return (
     <div className="app">
       <Topbar />
-      <div className="section">
+      <div className="sections">
         <Intro />
         <Portforlio />
         <Works />
