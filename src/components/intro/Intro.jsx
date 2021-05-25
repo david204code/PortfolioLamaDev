cd7ba420f4beba82423d3e8efd4bb4d2e5aff7b7
David Wu <david204code@gmail.com>
2021-05-25 18:34:45 +0100

    Styled the right side of the intro with animation and an arrow image

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -9,7 +9,18 @@ export default function Intro() {
           <img src="assets/man.png" alt="Picture of a man" />
         </div>
       </div>
-      <div className="right">right</div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Jonathan Sword</h1>
+          <h3>
+            Freelance<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="arrow" />
+        </a>
+      </div>
     </div>
   );
 }
