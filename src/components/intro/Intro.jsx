a608a4d37786abba4491bb84e0612a5869f10594
David Wu <david204code@gmail.com>
2021-05-26 15:37:29 +0100

    using useRef of react with ityped extention, intro finished

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  // instead of componentDidMount() use useEffect
+  useEffect(() => {
+    // console.log(textRef);
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 40,
+      strings: [
+        "Love the Lord your God",
+        "with all your Heart",
+        "your Soul",
+        "your Strength",
+      ],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +34,7 @@ export default function Intro() {
           <h2>Hi There, I'm</h2>
           <h1>David Wu</h1>
           <h3>
-            Ambassador of Christ <span>Worshipper</span>
+            Ambassador of Christ <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
