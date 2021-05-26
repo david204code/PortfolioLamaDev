23f82e21bbc9419f888458a19717d0f95c07134d
David Wu <david204code@gmail.com>
2021-05-26 16:10:14 +0100

    Add content and style to the portfoliio, flex, hover, opacity, cursor:pointer, margin, border, border-radius, display:flex

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,58 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      name
+      <h1>Porfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://w7.pngwing.com/pngs/271/19/png-transparent-starling-bank-challenger-bank-business-financial-services-bank-purple-company-violet-thumbnail.png"
+            alt="bank logo"
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://w7.pngwing.com/pngs/271/19/png-transparent-starling-bank-challenger-bank-business-financial-services-bank-purple-company-violet-thumbnail.png"
+            alt="bank logo"
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://w7.pngwing.com/pngs/271/19/png-transparent-starling-bank-challenger-bank-business-financial-services-bank-purple-company-violet-thumbnail.png"
+            alt="bank logo"
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://w7.pngwing.com/pngs/271/19/png-transparent-starling-bank-challenger-bank-business-financial-services-bank-purple-company-violet-thumbnail.png"
+            alt="bank logo"
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://w7.pngwing.com/pngs/271/19/png-transparent-starling-bank-challenger-bank-business-financial-services-bank-purple-company-violet-thumbnail.png"
+            alt="bank logo"
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://w7.pngwing.com/pngs/271/19/png-transparent-starling-bank-challenger-bank-business-financial-services-bank-purple-company-violet-thumbnail.png"
+            alt="bank logo"
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 }
