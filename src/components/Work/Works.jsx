a047be20e5b4177ebe7f91ffaca92ab255a85aab
David Wu <david204code@gmail.com>
2021-05-27 20:18:39 +0100

    Slider design with content and CSS style

--- src/components/Work/Works.jsx
+++ src/components/Work/Works.jsx
@@ -4,7 +4,31 @@ import "./works.scss";
 export default function Works() {
   return (
     <div className="works" id="works">
-      Works
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="mobile pic" />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Expedita vitae, ab tempora ducimus ipsum distinctio.
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+              <img
+                src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
