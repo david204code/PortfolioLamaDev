73022f95f0c52b1984a24739aa09c67e82fdb053
David Wu <david204code@gmail.com>
2021-05-27 20:42:43 +0100

    Dynamically rendering the data for display

--- src/components/Work/Works.jsx
+++ src/components/Work/Works.jsx
@@ -30,29 +30,28 @@ export default function Works() {
   return (
     <div className="works" id="works">
       <div className="slider">
-        <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src="assets/mobile.png" alt="mobile pic" />
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="mobile pic" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
                 </div>
-                <h2>Title</h2>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Expedita vitae, ab tempora ducimus ipsum distinctio.
-                </p>
-                <span>Projects</span>
+              </div>
+              <div className="right">
+                <img
+                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                  alt=""
+                />
               </div>
             </div>
-            <div className="right">
-              <img
-                src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
-                alt=""
-              />
-            </div>
           </div>
-        </div>
+        ))}
       </div>
       <img src="assets/arrow.png" className="arrow left" alt="" />
       <img src="assets/arrow.png" className="arrow right" alt="" />
