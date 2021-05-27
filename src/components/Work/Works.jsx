9dedf74c3d08613056c3c0e5a7b50cc114c4e3e6
David Wu <david204code@gmail.com>
2021-05-27 20:57:22 +0100

    Full functioning slider with onClick handleClick function along with the logic

--- src/components/Work/Works.jsx
+++ src/components/Work/Works.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./works.scss";
 
 export default function Works() {
+  // state for the slider
+  const [currentSlide, setCurrentSlide] = useState(0);
   // dummy data
   const data = [
     {
@@ -27,9 +29,19 @@ export default function Works() {
     },
   ];
 
+  // handleClick function
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
   return (
     <div className="works" id="works">
-      <div className="slider">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         {data.map((d) => (
           <div className="container">
             <div className="item">
@@ -53,8 +65,18 @@ export default function Works() {
           </div>
         ))}
       </div>
-      <img src="assets/arrow.png" className="arrow left" alt="" />
-      <img src="assets/arrow.png" className="arrow right" alt="" />
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick("right")}
+      />
     </div>
   );
 }
