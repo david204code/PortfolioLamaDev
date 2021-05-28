4e9b9d50915a7198fbb2b71fba787bf0c2bababe
David Wu <david204code@gmail.com>
2021-05-28 19:52:43 +0100

    Render the data for the card dynamically from the dummy data and FIXED the key REACT WARNING

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -35,26 +35,20 @@ export default function Testimonials() {
     <div className="testimonials" id="testimonials">
       <h1>Testimonials</h1>
       <div className="container">
-        <div className="card">
-          <div className="top">
-            <img src="assets/right-arrow.png" className="left" alt="" />
-            <img
-              className="user"
-              src="https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-            />
-            <img src="assets/youtube.png" className="right" alt="" />
+        {data.map((d) => (
+          <div className="card" key={d.id}>
+            <div className="top">
+              <img src="assets/right-arrow.png" className="left" alt="" />
+              <img className="user" src={d.img} alt="" />
+              <img src={d.icon} className="right" alt="" />
+            </div>
+            <div className="center">{d.desc}</div>
+            <div className="bottom">
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
           </div>
-          <div className="center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-            accusamus et sequi recusandae amet illum dolor earum ad quasi
-            corrupti.
-          </div>
-          <div className="bottom">
-            <h3>Matthew</h3>
-            <h4>Writer</h4>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
