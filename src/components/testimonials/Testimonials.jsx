c0f7e0bd1f4203a00fab778af3fd0bb5b644769b
David Wu <david204code@gmail.com>
2021-05-28 19:19:02 +0100

    Added HTML tags/content to Testimonails.jsx

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,7 +4,28 @@ import "./testimonials.scss";
 export default function Testimonials() {
   return (
     <div className="testimonials" id="testimonials">
-      Testimonials
+      <h1>Testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            <img src="assets/right-arrow.png" className="left" alt="" />
+            <img
+              src="https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <img src="assets/youtube.png" alt="" />
+          </div>
+          <div className="center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            accusamus et sequi recusandae amet illum dolor earum ad quasi
+            corrupti.
+          </div>
+          <div className="bottom">
+            <h3>Matthew</h3>
+            <h4>Writer</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
